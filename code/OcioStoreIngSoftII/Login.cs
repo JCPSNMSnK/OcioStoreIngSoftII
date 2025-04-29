73bfffee482d2f0ba7256a470a6a306e3bbf5410
Giovanni Piazza <gpiazza1804@gmail.com>
2025-04-28 23:57:38 -0300

    nuget

--- code/OcioStoreIngSoftII/Login.cs
+++ code/OcioStoreIngSoftII/Login.cs
@@ -10,7 +10,6 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using BCrypt.Net;
 using OcioStoreIngSoftII;
-
 using CapaEntidades;
 using CapaNegocio;
 
@@ -71,62 +70,57 @@ namespace OcioStoreIngSoftII
                 MessageBox.Show("No se encontro el usuario", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
-            //string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["pruebaLogin.Properties.Settings.db_piazza_giovanniConnectionString"].ConnectionString;
-            //string usuario = TUser.Text;
-            //string pass = TPass.Text;
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["cadena_conexion"].ConnectionString;
+            string usuario = TUser.Text;
+            string pass = TPass.Text;
 
-            //using (SqlConnection connection = new SqlConnection(connectionString))
-            //{
-            //    connection.Open();
-            //    string query = "SELECT pass, perfil_id FROM usuarios WHERE usuario=@usuario";
-            //    SqlCommand cmd = new SqlCommand(query, connection);
-            //    cmd.Parameters.AddWithValue("@usuario", usuario);
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT pass, id_rol FROM users WHERE username=@usuario";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@usuario", usuario);
 
-            //    using (SqlDataReader reader = cmd.ExecuteReader())
-            //    {
-            //        if (reader.Read()) // Si el usuario existe
-            //        {
-            //            string storedHash = reader["pass"].ToString();
-            //            int perfilId = Convert.ToInt32(reader["perfil_id"]);
-            //            Usuario ousuario = new Usuario_negocio().Listar().Where(u => u.user == TUser.Text && u.pass == storedHash).FirstOrDefault();
-            //            Inicio form_inicio = new Inicio(ousuario);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read()) // Si el usuario existe
+                    {
+                        string storedPassword = reader["pass"].ToString();
+                        int perfilId = Convert.ToInt32(reader["id_rol"]);
+                        Usuario ousuario = new Usuario_negocio().Listar()
+                            .Where(u => u.user == TUser.Text && u.pass == storedPassword)
+                            .FirstOrDefault();
+                        Inicio form_inicio = new Inicio(ousuario);
 
-            //            // Verificamos si la contraseña está hasheada o en texto claro
-            //            bool isAuthenticated = false;
-            //            if (storedHash.Length == 60) // Longitud típica de un hash BCrypt
-            //            {
-            //                isAuthenticated = BCrypt.Net.BCrypt.Verify(pass, storedHash);
-            //            }
-            //            else
-            //            {
-            //                isAuthenticated = pass == storedHash; // Compara directamente si no está hasheada
-            //            }
+                        // Solo comparamos directamente las contraseñas
+                        bool isAuthenticated = pass == storedPassword;
 
-            //            // Verificamos si la autenticación fue exitosa y que el perfil_id sea distinto de 0
-            //            if (isAuthenticated && perfilId != 0)
-            //            {
-            //                // Caso de éxito: Mostrar la nueva ventana y ocultar la actual
-            //                form_inicio.Show();
-            //                this.Hide();
+                        // Verificamos si la autenticación fue exitosa y que el perfil_id sea distinto de 0
+                        if (isAuthenticated && perfilId != 0)
+                        {
+                            // Caso de éxito: Mostrar la nueva ventana y ocultar la actual
+                            form_inicio.Show();
+                            this.Hide();
 
-            //                // Aseguramos que el evento form_closing se suscribe antes de mostrar el formulario
-            //                form_inicio.FormClosing += form_closing;
-            //            }
-            //            else if (perfilId == 0)
-            //            {
-            //                MessageBox.Show("El usuario no tiene permiso para ingresar");
-            //            }
-            //            else
-            //            {
-            //                MessageBox.Show("Usuario o Contraseña no válidos");
-            //            }
-            //        }
-            //        else
-            //        {
-            //            MessageBox.Show("Usuario o Contraseña no válidos");
-            //        }
-            //    }
-            //}
+                            // Aseguramos que el evento form_closing se suscribe antes de mostrar el formulario
+                            form_inicio.FormClosing += form_closing;
+                        }
+                        else if (perfilId == 0)
+                        {
+                            MessageBox.Show("El usuario no tiene permiso para ingresar");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Usuario o Contraseña no válidos");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario o Contraseña no válidos");
+                    }
+
+                }
+            }
         }
 
         public void form_closing(object sender, FormClosingEventArgs e)
