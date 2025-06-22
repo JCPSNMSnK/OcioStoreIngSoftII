d3b77281157817eb7c14a6bd18fecf0e35ddfb63
joelmarcori1 <joelmarcori1@gmail.com>
2025-06-22 11:57:14 -0300

    Restauradas las entidades intermedias.
    No sé porqué borraron estas entidades xd.

---
+++ code/CapaEntidades/ProductosCategorias.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class ProductosCategorias
+    {
+        public Producto objProducto { get; set; }
+        public Categoria objCategoria { get; set; }
+    }
+}
