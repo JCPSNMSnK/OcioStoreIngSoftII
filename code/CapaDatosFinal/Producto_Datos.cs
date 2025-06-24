2f2b3a9b51cd065b39e60675517d745258a132af
Julio Cesar Pintos <juliopintoscesar@gmail.com>
2025-06-24 02:55:31 -0300

    restaurando version funcional

--- code/CapaDatosFinal/Producto_Datos.cs
+++ code/CapaDatosFinal/Producto_Datos.cs
@@ -16,7 +16,7 @@ namespace CapaDatos
     {
 
         // El método Listar ahora acepta todos los parámetros de búsqueda como opcionales
-        public List<Producto> Listar(
+        /*public List<Producto> Listar(
             //out string mensaje,
             int? id_producto = null,
             string nombre_producto = null,
@@ -126,6 +126,113 @@ namespace CapaDatos
             }
             // Devuelve la lista de productos únicos (los valores del diccionario)
             return productosMap.Values.ToList();
+        }*/
+        //esta mejorar no se pudo implementar aún
+
+        public List<Producto> Listar()//mostrarProductos
+
+        {
+
+            List<Producto> lista = new List<Producto>();
+
+
+
+            using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+
+            {
+
+                try
+
+                {
+
+                    StringBuilder query = new StringBuilder();
+
+                    query.AppendLine("SELECT p.id_producto, p.nombre_producto, p.fechaIngreso, p.descripcion, p.precioLista, p.precioVenta,");
+
+                    query.AppendLine("p.stock, p.stock_min, p.baja_producto, c.id_categoria, c.nombre_categoria");
+
+                    query.AppendLine("FROM Productos p ");
+
+                    query.AppendLine("LEFT JOIN ProductosCategorias pc ON p.id_producto = pc.id_producto ");
+
+                    query.AppendLine("LEFT JOIN Categorias c ON pc.id_categoria = c.id_categoria ");
+
+                    //query.AppendLine("WHERE p.baja_producto = 0"); // Filtro para productos activos
+
+
+
+                    SqlCommand cmd = new SqlCommand(query.ToString(), oconexion);
+
+                    cmd.CommandType = CommandType.Text;
+
+
+
+                    oconexion.Open();
+
+
+
+                    using (SqlDataReader dataReader = cmd.ExecuteReader())
+
+                    {
+
+                        while (dataReader.Read())
+
+                        {
+
+                            lista.Add(new Producto()
+
+                            {
+
+                                id_producto = Convert.ToInt32(dataReader["id_producto"]),
+
+                                nombre_producto = dataReader["nombre_producto"].ToString(),
+
+                                fechaIngreso = dataReader["fechaIngreso"] != DBNull.Value
+
+                                ? Convert.ToDateTime(dataReader["fechaIngreso"])
+
+                                : DateTime.Now,
+
+
+
+                                precioLista = Convert.ToDecimal(dataReader["precioLista"]),
+
+                                precioVenta = Convert.ToDecimal(dataReader["precioVenta"]),
+
+                                baja_producto = Convert.ToBoolean(dataReader["baja_producto"]),
+
+                                stock_min = Convert.ToInt32(dataReader["stock_min"]),
+
+                                stock = Convert.ToInt32(dataReader["stock"]),
+
+                                descripcion = dataReader["descripcion"] != DBNull.Value
+
+                                    ? dataReader["descripcion"].ToString()
+
+                                    : string.Empty,
+
+                            });
+
+                        }
+
+                    }
+
+                }
+
+                catch (Exception ex)
+
+                {
+
+                    lista = new List<Producto>();
+
+                    string Mensaje = ex.Message;
+
+                }
+
+            }
+
+            return lista;
+
         }
 
         public int Registrar(Producto obj, Categoria objCat, out string Mensaje)//crearProducto
